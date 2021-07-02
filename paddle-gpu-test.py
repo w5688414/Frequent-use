ca269090e50c826c05b907eccc0e9ae34f96b39a
w5688414 <w5688414@gmail.com>
2021-07-02 16:30:44 +0800

    paddle gpu test

---
+++ paddle-gpu-test.py
@@ -0,0 +1,20 @@
+import paddle
+print("paddle " + paddle.__version__)
+
+
+x_data = paddle.to_tensor([[1.], [3.0], [5.0], [9.0], [10.0], [20.0]])
+y_data = paddle.to_tensor([[12.], [16.0], [20.0], [28.0], [30.0], [50.0]])
+
+
+linear = paddle.nn.Linear(in_features=1, out_features=1)
+
+
+w_before_opt = linear.weight.numpy().item()
+b_before_opt = linear.bias.numpy().item()
+
+print("w before optimize: {}".format(w_before_opt))
+print("b before optimize: {}".format(b_before_opt))
+
+
+device = paddle.get_device()
+print(device)
